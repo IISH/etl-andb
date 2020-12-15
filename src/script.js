5968c73f92325d4122fa73becf6483673af36ce0
Wouter Beek <wouter@triply.cc>
2020-12-15 14:22:37 +0100

    up

--- src/script.js
+++ src/script.js
@@ -2,22 +2,19 @@ const fs = require('fs')
 const factory = require('rdf-ext')
 const ParserN3 = require('@rdfjs/parser-n3')
 const SHACLValidator = require('rdf-validate-shacl')
-
 async function loadDataset (filePath) {
   const stream = fs.createReadStream(filePath)
   const parser = new ParserN3({ factory })
   return factory.dataset().import(parser.import(stream))
 }
 const run = async () => {
-  const shapes = await loadDataset('../rdf/shapes.trig')
-  const data = await loadDataset('../rdf/example.trig')
+  const shapes = await loadDataset('bag-shapes.trig')
+  const data = await loadDataset('bag-voorbeeld.trig')
   const validator = new SHACLValidator(shapes, { factory })
+  console.log(validator)
   const report = await validator.validate(data)
-  // Check conformance: `true` or `false`
-  console.log(report.conforms)
+  console.log(report)
   for (const result of report.results) {
-    // See https://www.w3.org/TR/shacl/#results-validation-result for details
-    // about each property
     console.log("  Message:\t" + result.message)
     console.log("  Path:\t\t" + result.path)
     console.log("  Focus node:\t" + result.focusNode)
