b5a6f525f55a31f9e3fd26e0858d9b18da6a5400
Wouter Beek <wouter@triply.cc>
2021-01-13 17:37:18 +0100

    CLEAN: Simplified script further.

--- jsf/convert.py
+++ jsf/convert.py
@@ -3,10 +3,3 @@ import glob
 
 for file in glob.glob("*.csv"):
     COW(mode='convert', files=[file])
-      #dataset='ADB',
-      #delimiter=',',
-      #quotechar='\"',
-      #processes=1,
-      #chunksize=2,
-      #base='https://iisg.amsterdam/id/adb',
-      #output_format='nquads')
